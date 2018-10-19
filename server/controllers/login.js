44846e4a82a605d0afecd251b50d26bac06ebd27
joshborup <joshborup@gmail.com>
2018-10-19 13:41:30 -0700

    updated auth

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -12,7 +12,7 @@ module.exports = {
               code: req.query.code,
               client_secret: process.env.AUTH0_CLIENT_SECRET,
               grant_type: 'authorization_code',
-              redirect_uri: `http://${req.headers.host}/callback`
+              redirect_uri: `https://${req.headers.host}/callback`
             }
             
             const exchangeCodeForAccessToken = () =>{
